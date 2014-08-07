d7af60a38fae11762b81ded85227c77205b7ff76
agmenc <chris.agmen-smith@email.com>
2014-08-07 09:17:57 +0100

    Start defining a tree diff test

---
+++ src/test/scala/planet7/tree/TreeDiffSpec.scala
@@ -0,0 +1,54 @@
+package planet7.tree
+
+import org.scalatest.WordSpec
+
+class TreeDiffSpec extends WordSpec {
+  "Comparing two JSON trees results in a tuple of two smaller diff trees" in {
+    val left =
+      """
+        |{
+        | "blaaaahh": "blah blah blah",
+        | "address": {
+        |   "street": "Main Street",
+        |   "postcode": "ABC123"
+        | },
+        | "name": "bob",
+        | "blah": "more blah"
+        |}
+      """.stripMargin
+
+    val right =
+      """
+        |{
+        | "blaaaahh": "blah blah blah",
+        | "address": {
+        |   "postcode": "ABC123"
+        | },
+        | "name": "fred",
+        | "DOB": 1987,
+        | "blah": "more blah"
+        |}
+      """.stripMargin
+
+    val expected = (
+      """
+        |{
+        | "address": {
+        |   "postcode": "ABC123"
+        | },
+        | "name": "bob"
+        |}
+      """.stripMargin,
+      """
+        |{
+        | "name": "fred",
+        | "DOB": 1987,
+        |}
+      """.stripMargin
+      )
+
+      // ADDED   = the diffs, filtered by "left is not on right"
+      // REMOVED = the diffs, filtered by "right is not on left"
+      // CHANGED = left != right
+  }
+}
