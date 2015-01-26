68883ec861600a5e07747dd850f29f8db8af9d3d
agmenc <chris.agmen-smith@email.com>
2015-01-26 08:51:10 +0000

    Improve doc comments in package object

--- src/main/scala/planet7/tabular/package.scala
+++ src/main/scala/planet7/tabular/package.scala
@@ -9,21 +9,22 @@ package object tabular extends DataSourceLoaders with DataSinks {
 
   def sort(csv: Csv, differ: RowDiffer): Csv = Csv(csv.header, csv.iterator.toSeq.sorted(differ.ordering).iterator)
 
+  /** Used in sort(input, "Age" -> by(_.toInt)) */
   def by[K: Ordering](f: String => K): Ordering[String] = new Ordering[String] {
     override def compare(x: String, y: String) = implicitly[Ordering[K]].compare(f(x), f(y))
   }
 
   def ignore(columnNames: String*) = (headerColumns: Array[String]) => headerColumns filter (col => !columnNames.contains(col))
 
-  /** Used in Csv.columnStructure(). Puts a single columnName String into a before/after Tuple. */
+  /** Converts Csv(data).columnStructure("Name") to Csv(data).columnStructure("Name" -> "Name") */
   implicit def toColumnStructure(s: String): (String, String) = s -> s
 
-  /** Used in sort(). Converts a single String into a Tuple with a basic Comparator[String]. */
+  /** Converts sort(input, "Surname") into sort(input, "Surname" -> Comparator[String]) */
   implicit def toStringCompare(s: String): (String, Comparator[String]) = s -> new Comparator[String] {
     override def compare(o1: String, o2: String) = o1.compareTo(o2)
   }
 
-  /** Used in Cvs.assertAndAbort() and Csv.assertAndReport(), to build simple validation checks */
+  /** Used in Cvs.assertAndAbort() and Csv.assertAndReport(). Converts , to build simple validation checks */
   implicit def toValidation(columnAssertion: (String, String => Boolean)): Row => Row => Row =
     (header: Row) => (row: Row) => {
       val (columnName, validationFunction) = columnAssertion
